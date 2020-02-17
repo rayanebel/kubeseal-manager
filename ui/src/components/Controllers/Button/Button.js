7cc4fc20ff78960b7dadef9179a1deb741af7262
rayanebel <rayane.bellazaar@gmail.com>
2020-02-17 14:46:32 +0100

    update front

--- ui/src/components/Controllers/Button/Button.js
+++ ui/src/components/Controllers/Button/Button.js
@@ -7,13 +7,15 @@ const button = props => {
   return (
     <div className={classes.Container}>
       <button className={classes.Button}>
-        <div className={classes.Icon}> 
-          <IconContext.Provider
-            value={{ style: { color: '#FCD04B' }, size: '20px' }}
-          >
-            <FaUserSecret />
-          </IconContext.Provider>
-        </div>
+        <IconContext.Provider
+          value={{
+            style: { color: '#FCD04B' },
+            size: '20px',
+            className: classes.Icon,
+          }}
+        >
+          <FaUserSecret />
+        </IconContext.Provider>
         <span className={classes.Text}>Seal</span>
       </button>
     </div>
